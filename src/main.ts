3cf7508d4dd9f376ae5338aec138e54f7fa9948b
Maulana Kayyis <maulanakayyis354@gmail.com>
2025-05-19 08:19:02 +0700

    fix(url): add frontend url cors

--- src/main.ts
+++ src/main.ts
@@ -29,7 +29,7 @@ async function bootstrap() {
   app.use(helmet());
   app.use(
     cors({
-      origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+      origin: process.env.FRONTEND_URL || 'http://localhost:3000' || 'https://gcb-frontend.vercel.app/',
       credentials: true,
     }),
   );
